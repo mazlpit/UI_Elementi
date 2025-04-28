d3c3aa82bd91543329e3e0ff68918b83e7150e15
Sauka <markuss.sauka@sk.lvt.lv>
2025-04-28 16:05:33 +0300

    End of lesson

---
+++ Assets/Scripts/ImageScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageScript : MonoBehaviour
+{
+    public GameObject bean;
+    public GameObject teddy;
+    public GameObject car;
+    public GameObject lady;
+    public GameObject left;
+    public GameObject right;
+
+    public void ToogleBean(bool value)
+    {
+        bean.SetActive(value);
+        left.GetComponent<Toggle>().interactable = value;
+        right.GetComponent<Toggle>().interactable = value;
+    }
+
+    public void ToogleTeddy(bool value)
+    {
+        teddy.SetActive(value);
+    }
+    public void ToogleCar(bool value)
+    {
+        car.SetActive(value);
+    }
+
+    public void ToogleLady(bool value)
+    {
+        lady.SetActive(value);
+    }
+
+    public void ToLeft()
+    {
+        bean.transform.localScale = new Vector2 (1, 1); 
+    }
+    public void ToRight()
+    {
+        bean.transform.localScale = new Vector2(-1, 1);
+    }
+
+}
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Pirmais Unity 2D projekts, apgūstam UI elementu lietošanu
 
 **Darāmo darbu saraksts:**
-- [ ] Button, Input field un text elementu lietojums
-- [ ] Canvas izmēra pielāgošana
+- [x] Button, Input field un text elementu lietojums
+- [x] Canvas izmēra pielāgošana
 - [ ] Checkbox, Dropdown un slider elementu lietojums
-- [ ] Pirmā C# skripta izveide
+- [x] Pirmā C# skripta izveide
 - [ ] Audio source ieviešana
 - [ ] Scrollview elementa lietojums
 - [ ] Otrās ainas (scene) ieviešana
